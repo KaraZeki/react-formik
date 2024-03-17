58a327d820ffb923a988b585ac692407f4cc3cad
Mehmet Zeki KARA <karamehmetzeki506@gmail.com>
2024-03-17 15:56:38 +0300

    react formik

--- src/components/GeneralForm.js
+++ src/components/GeneralForm.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Link } from "react-router-dom";
 
 export const GeneralForm = () => {
+  const colors = ["Sarı", "Mavi", "Yeşil", "Kırmız", "Mor"];
   return (
     <div>
       <h1>Kayıt Ol</h1>
@@ -26,8 +28,7 @@ export const GeneralForm = () => {
           }
           if (!values.password) {
             errors.password = "Zorunlu alan";
-          } 
-          else if (values.password != values.confirmPassword) {
+          } else if (values.password != values.confirmPassword) {
             errors.confirmPassword = "Şifreler eşleşmiyor kardeş";
           }
           if (!values.age) {
@@ -37,11 +38,12 @@ export const GeneralForm = () => {
           }
           return errors;
         }}
-        onSubmit={(values, { setSubmitting }) => {
+        onSubmit={(values, { setSubmitting, resetForm }) => {
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
             setSubmitting(false);
-          }, 400);
+            resetForm();
+          }, 5000);
         }}
       >
         {({ isSubmitting }) => (
@@ -54,13 +56,38 @@ export const GeneralForm = () => {
             />
             <Field type="text" name="age" placeholder="yaş giriniz" />
             <ErrorMessage className="errormessage" name="age" component="div" />
-            <Field type="password" name="password" />
-            <ErrorMessage name="password" component="div" />
-            <Field type="password" name="confirmPassword" />
-            <ErrorMessage className="errormessage" name="confirmPassword" component="div" />
+            <Field
+              type="password"
+              name="password"
+              placeholder="Şifre giriniz"
+            />
+            <ErrorMessage
+              className="errormessage"
+              name="password"
+              component="div"
+            />
+            <Field
+              type="password"
+              name="confirmPassword"
+              placeholder="Tekrar şifre giriniz"
+            />
+            <ErrorMessage
+              className="errormessage"
+              name="confirmPassword"
+              component="div"
+            />
+
+            <Field name="colors" as="select" className="my-select">
+              {colors.map((color) => {
+                return <option value={color}>{color}</option>;
+              })}
+            </Field>
             <button type="submit" disabled={isSubmitting}>
               Kaydet
             </button>
+            <Link className="form-link" to="/portal">
+              Portala Git
+            </Link>
           </Form>
         )}
       </Formik>
--- src/components/PortalForm.js
+++ src/components/PortalForm.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { Link } from "react-router-dom";
 
 export const PortalForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, "15 karakter veya daha az olmalıdır")
+        .required("Zorunlu"),
+      lastName: Yup.string()
+        .max(20, "20 karakter veya daha az olmalıdır")
+        .required("Zorunlu"),
+      email: Yup.string().email("Geçersiz email adres").required("Zorunlu"),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
-    <div>PortalForm</div>
-  )
-}
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">Ad</label>
+      <input
+        id="firstName"
+        type="text"
+        {...formik.getFieldProps("firstName")}
+      />
+      {formik.touched.firstName && formik.errors.firstName ? (
+        <div className="errormessage">{formik.errors.firstName}</div>
+      ) : null}
+
+      <label htmlFor="lastName">Soyad</label>
+      <input id="lastName" type="text" {...formik.getFieldProps("lastName")} />
+      {formik.touched.lastName && formik.errors.lastName ? (
+        <div className="errormessage">{formik.errors.lastName}</div>
+      ) : null}
+
+      <label htmlFor="email">Email Adres</label>
+      <input id="email" type="email" {...formik.getFieldProps("email")} />
+      {formik.touched.email && formik.errors.email ? (
+        <div className="errormessage">{formik.errors.email}</div>
+      ) : null}
+
+      <button type="submit">Gönder</button>
+      <Link className="form-link" to="/">
+        Anasayfa Git
+      </Link>
+    </form>
+  );
+};
